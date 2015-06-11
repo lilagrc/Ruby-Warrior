35671bbf52c9ff5faeb1fa48011fe1d8a87a2762
Lila Garcia <lilagrc@gmail.com>
2015-06-10 21:04:06 -0700

    some more level 6 code

--- beginner:level6.rb
+++ beginner:level6.rb
@@ -24,5 +24,41 @@
   end
 end
 
-#can set initialize, can set health to 20
 
+
+MAX_HEALTH = 20
+  LOW_HEALTH <= 10
+
+  def play_turn(warrior)
+    MAX_HEALTH = @health
+    if warrior.feel.empty?
+      warrior.walk!(:backward)
+      if warrior.feel.captive?(:backward)
+        warrior.rescue!
+      end
+   elsif warrior.feel.empty? == false
+      if warrior.feel.enemy?
+        if @health == LOW_HEALTH
+          warrior.retreat!
+        else warrior.attack!
+        end
+      end
+    end
+    end
+    @end_health = warrior.health
+  end
+
+  def taking_damage?
+    if @end_health > @health
+      true
+    else
+      false
+  end
+
+  def retreat!
+    if taking_damage? == true
+      warrior.walk!(:backward)
+    else
+      warrior.rest!
+  end
+end
